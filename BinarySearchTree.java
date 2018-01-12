51a42f8ac8c99701292ff26ca098e5312a7857ea
arnabruteforcer <30342252+arnabruteforcer@users.noreply.github.com>
2018-01-12 18:43:06 +0530

    Update BinarySearchTree.java

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,5 +1,6 @@
 package mytree;
 
+
 public class BinarySearchTree<T extends Comparable<T>> implements Tree<T>{
 
 	private TreeNode<T> root;
@@ -19,7 +20,6 @@ public class BinarySearchTree<T extends Comparable<T>> implements Tree<T>{
 		}
 		
 	}
-	
 	
 	private void insert(T data, TreeNode<T> ptr)
 	{
@@ -42,22 +42,20 @@ public class BinarySearchTree<T extends Comparable<T>> implements Tree<T>{
 		   {
 			   TreeNode<T> newNode = new TreeNode<>();
 			   newNode.setKey(data);
-			   ptr.setLeft(newNode);
-			   
+			   ptr.setLeft(newNode); 
 		   }
-		   
 		   else insert( data , ptr.getLeft() );
 	   }
 	   
 	}   // end of insert....
 
+	
 	@Override
 	public void inorderTraversal() {
 		
 		if(root == null){
 			System.out.println("Empty tree");
-		}
-		
+		}	
 		else{
 			inorder(root);
 		}
@@ -69,10 +67,111 @@ public class BinarySearchTree<T extends Comparable<T>> implements Tree<T>{
 			return ;
 		
 	inorder(ptr.getLeft());               // recursing down to the left subtree
-	System.out.println(ptr.getKey() + " ");
+	System.out.print(ptr.getKey() + " ");
 	inorder(ptr.getRight());
 	
 	}
 	
-  
-}             /* end of binarySearchTree class  */
+	
+	public void minNode()
+	{
+		if (root == null){
+			System.out.println("empty tree");
+		}
+		
+		System.out.println("The minimum node is " + getMin(root));
+	}
+	
+	
+	/* traverse down towards the left until u get to the bottom */
+	/* node which will be the smallest*/
+	private T getMin(TreeNode<T> node)
+	{
+		if(node.getLeft() ==  null){
+			return node.getKey();
+		}
+		return getMin(node.getLeft());
+	}
+	
+	/* traverse down towards the right until u get to the bottom */
+	/* node which will be the largest*/
+	
+	public void getMax()
+	{
+		if( root == null){
+			System.out.println("Empty Tree");
+		}
+		else{
+			TreeNode<T> node = root;
+			
+			while(node.hasRightchild()){
+				node = node.getRight();
+			}
+			System.out.println("Max node is " + node.getKey());
+		}  		 							  // end of else
+	
+	}        			  /* end of method getMax().......*/
+	
+	
+	public void deleteKey(T data)
+	{
+		if(root == null){
+			System.out.println("Empty tree");
+		}
+		else
+		{
+			root = deleteNode(data,root,root);
+		}
+	}
+
+	   												/* as it is an unthreaded tree we need to have a parent pointer to store the data */ 
+	
+	private TreeNode<T> deleteNode(T data, TreeNode<T> parent,TreeNode<T> node) {	
+		
+		if( node == null )                                 /* if we reached the end of tree */
+		{
+			System.out.println("key not found");
+		}
+		
+		
+		else{
+			
+			if( node.getKey().compareTo(data) == 0 )         /* we have found the key to be deleted */
+			{
+			    int side = -1;         
+			    
+				if(parent.getLeft() == node)
+					side = 0;                                  // 0 corresponds for the left child
+				else
+					parent.setRight(null);
+				
+				
+				if(!node.hasLeftchild() && !node.hasRightchild()){   /*case1: if its a leaf node ,set the left/right child for the parent pointer to be null*/
+					
+					
+				}          								/* end of if for the node found..... */
+				
+				else if ( node.hasLeftchild()){
+					
+				}
+				
+				
+			}
+			
+			else if( node.getKey().compareTo(data)>0 ){         /* if search key is smaller than current node value  we search on the left of current node*/
+			   return ( deleteNode(data, node, node.getLeft()) );
+		    }
+		   
+		   else{
+			   return ( deleteNode(data, node, node.getRight()) );			/* if search key is greater than current node value  we search on the left of current node*/
+		   }
+		  
+		
+		}                    										/* end of outer else....... */
+	
+		return root;
+		
+	}																			/* end of deleteNode method....... */
+	
+	
+}            			 /* end of binarySearchTree class  */
